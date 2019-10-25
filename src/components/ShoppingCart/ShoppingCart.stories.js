f354a44a17424f1dbea5303f6c899ed300fdbc33
Jon Q <hello@jonquach.com>
2019-10-25 14:12:22 -0400

    First thing

---
+++ src/components/ShoppingCart/ShoppingCart.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ShoppingCart from './index';
+
+export default {
+	title: 'ShoppingCart',
+};
+
+export const _default = () => {
+	return <ShoppingCart />;
+};
---
+++ src/components/ShoppingCart/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './custom.css';
+import './ShoppingCart.css';
+
+export default function ShoppingCart() {
+	/**
+	 * ADD HTML PLEASE! THANK YOU!
+	 */
+	return (
+		<div className="ShoppingCard">
+			<div className="title">Order</div>
+			<div className="item">
+				<div className="thumbnail">
+					<img src="" />
+				</div>
+				<div className="content">
+					<div className="details">
+						<h3>Item 1</h3>
+						<p>Stuff</p>
+					</div>
+					<div className="plus-minus">
+						<button className="btn btn-outline-secondary">-</button>
+						<input className="form-control" />
+						<button className="btn btn-outline-secondary">+</button>
+					</div>
+					<div className="price">$20.00</div>
+				</div>
+			</div>
+		</div>
+	);
+}
